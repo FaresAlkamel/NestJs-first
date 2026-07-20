b20de2e33dc1942a57ea81d63161f69676170dcb
FaresAlkamel <alkamel.fares2@gmail.com>
2026-07-20 20:47:13 +0300

    done middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
-import { PhotoModule } from './photo/photo.module';
+import {SongsController} from './songs/songs.controller';
+import { NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 
 @Module({
-  imports: [SongsModule, PhotoModule],
+  imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer : MiddlewareConsumer){
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController);
+    //ther is too many options for the routes, you can use the following options:
+    //forRoutes('songs') // for all routes that start with 'songs'
+    //forRoutes({path: 'songs', method: RequestMethod.GET}) // for specific route and method
+    //forRoutes({path: 'songs', method: RequestMethod.POST}) // for specific route and method
+  }
+}
